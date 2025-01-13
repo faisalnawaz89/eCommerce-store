fb44163c94963decc772d8aeae331bd425bbb92d
Faisal Nawaz <isfaisalnawaz@gmail.com>
2025-01-13 22:05:20 +0530

    fix typo error

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
             <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-                <span>Conatct</span>
+                <span>Contact</span>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
         </ul>
